44d48e4f530a29f48c6f31391e68c5870d25fa21
BhargavMaru07 <bhargavhamir@gmail.com>
2024-12-11 15:33:53 +0530

    Add product and get product and delete & update product

--- src/context/data/MyState.jsx
+++ src/context/data/MyState.jsx
@@ -1,9 +1,23 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import MyContext from "./MyContext";
+import {
+  addDoc,
+  collection,
+  onSnapshot,
+  orderBy,
+  query,
+  Timestamp,
+  QuerySnapshot,
+  doc,
+  deleteDoc,
+  setDoc,
+} from "firebase/firestore";
+import { toast } from "react-toastify";
+import { fireDB } from "../../firebase/FirebaseConfig";
 
 export const MyState = ({ children }) => {
   let [mode, setMode] = useState("light");
-  let [loading,setLoading] = useState(false)
+  let [loading, setLoading] = useState(false);
 
   let toggleMode = () => {
     if (mode === "light") {
@@ -11,13 +25,188 @@ export const MyState = ({ children }) => {
       document.body.style.backgroundColor = "rgb(17, 24, 39)";
     } else {
       setMode("light");
-      document.body.style.backgroundColor="white";
+      document.body.style.backgroundColor = "white";
+    }
+  };
+
+  let [products, setProducts] = useState({
+    title: "",
+    price: "",
+    imageUrl: "",
+    category: "",
+    description: "",
+    time: Timestamp.now(),
+    date: new Date().toLocaleString("en-US", {
+      month: "short",
+      day: "2-digit",
+      year: "numeric",
+    }),
+  });
+
+  // ********************** Add Product Section  **********************
+
+  const addProduct = async () => {
+    setLoading(true);
+    if (
+      products.title == "" ||
+      products.price == "" ||
+      products.imageUrl == "" ||
+      products.category == "" ||
+      products.description == ""
+    ) {
+      setLoading(false);
+      return toast.error("All Fields are required");
+    }
+
+    const productRef = collection(fireDB, "products");
+
+    try {
+      await addDoc(productRef, products);
+      toast.success("Product Add successfully");
+
+      setProducts({
+        ...products,
+        title: "",
+        price: "",
+        imageUrl: "",
+        category: "",
+        description: "",
+      });
+
+      getProductData();
+
+      setTimeout(() => {
+        window.location.href = "/dashboard";
+      }, 1500);
+    } catch (error) {
+      console.log(error);
+      toast.error("Error Occurred In AddProduct");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  let [product, setProduct] = useState([]);
+
+  //*********get Product
+
+  const getProductData = async () => {
+    setLoading(true);
+
+    try {
+      const q = query(collection(fireDB, "products"), orderBy("time"));
+
+      //onSnapshot explaination below...
+      const data = onSnapshot(q, (QuerySnapshot) => {
+        let productsArray = [];
+
+        QuerySnapshot.forEach((doc) => {
+          productsArray.push({ ...doc.data(), id: doc.id });
+        });
+
+        setProduct(productsArray);
+        setLoading(false);
+      });
+
+      return () => data; //it's explanation below
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+      toast.error("Error Occurred In GetProductData");
+    }
+  };
+
+  useEffect(() => {
+    getProductData();
+  }, []);
+
+  //editHandle....................
+
+  let editHandle = (item)=>{
+    setProducts(item)
+  }
+
+
+  //Update Products................
+
+  let updateProduct = async (item) => {
+    setLoading(true);
+    try {
+      await setDoc(doc(fireDB, "products", products.id), products);
+      toast.success("Product Updated successfully");
+      getProductData();
+      setTimeout(() => {
+        window.location.href = "/dashboard";
+      }, 1500);
+    } catch (error) {
+      console.log(error);
+      toast.error("Error!!!!!!!");
+    } finally {
+      setLoading(false);
+      setProducts({
+        ...products,
+        title: "",
+        price: "",
+        imageUrl: "",
+        category: "",
+        description: "",
+      });
+    }
+  };
+
+  //delete Products....................
+
+  let deleteProduct = async (item) => {
+    setLoading(true);
+    try {
+      await deleteDoc(doc(fireDB, "products", item.id));
+      toast.success("Product Deleted Successfully!!");
+      getProductData();
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+      toast.error("Error in deleteProduct!!!!!");
     }
   };
   return (
     //for multiple value, we create object and wrape all values in it
-    <MyContext.Provider value={{ mode, toggleMode, loading, setLoading }}>
+    <MyContext.Provider
+      value={{
+        mode,
+        toggleMode,
+        loading,
+        setLoading,
+        products,
+        setProducts,
+        addProduct,
+        product,
+        editHandle,
+        updateProduct,
+        deleteProduct,
+      }}
+    >
       {children}
-    </MyContext.Provider> 
+    </MyContext.Provider>
   );
 };
+
+/* 
+Why is return () => data Used?
+
+The onSnapshot method sets up a real-time listener to Firestore, meaning it listens for updates and triggers the callback whenever the data changes.
+
+Firestore provides a listener unsubscribe function when you call onSnapshot. This function (data in your code) is used to detach the listener when it is no longer needed.
+
+Key Purpose
+
+This cleanup function (return () => data) ensures that when the component using this listener is unmounted, the real-time listener is unsubscribed.
+
+Without it, the listener will remain active even if the component is removed, causing memory leaks or unwanted updates to a non-existent component.
+
+How It Works
+
+When getProductData is called, onSnapshot sets up a Firestore listener and assigns the unsubscribe function to data.
+The return () => data ensures that the listener is cleaned up when the component unmounts.
+
+*/
--- src/pages/admin/page/UpdateProduct.jsx
+++ src/pages/admin/page/UpdateProduct.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
+import MyContext from "../../../context/data/MyContext";
+import Loading2 from "../../../components/loader/Loader2"
 
 function UpdateProduct() {
+
+   let context = useContext(MyContext);
+   let { products, setProducts, updateProduct, loading } = context;
   return (
     <div>
       <div className=" flex justify-center items-center h-screen">
+        {loading && <Loading2 />}
         <div className=" bg-gray-800 px-10 py-10 rounded-xl ">
           <div className="">
             <h1 className="text-center text-white text-xl mb-4 font-bold">
@@ -12,6 +18,10 @@ function UpdateProduct() {
           </div>
           <div>
             <input
+              value={products.title}
+              onChange={(e) =>
+                setProducts({ ...products, title: e.target.value })
+              }
               type="text"
               name="title"
               className=" bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none"
@@ -20,6 +30,10 @@ function UpdateProduct() {
           </div>
           <div>
             <input
+              value={products.price}
+              onChange={(e) =>
+                setProducts({ ...products, price: e.target.value })
+              }
               type="text"
               name="price"
               className=" bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none"
@@ -28,6 +42,10 @@ function UpdateProduct() {
           </div>
           <div>
             <input
+              value={products.imageUrl}
+              onChange={(e) =>
+                setProducts({ ...products, imageUrl: e.target.value })
+              }
               type="text"
               name="imageurl"
               className=" bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none"
@@ -36,6 +54,10 @@ function UpdateProduct() {
           </div>
           <div>
             <input
+              value={products.category}
+              onChange={(e) =>
+                setProducts({ ...products, category: e.target.value })
+              }
               type="text"
               name="category"
               className=" bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none"
@@ -44,15 +66,19 @@ function UpdateProduct() {
           </div>
           <div>
             <textarea
+              value={products.description}
+              onChange={(e) =>
+                setProducts({ ...products, description: e.target.value })
+              }
               cols="30"
               rows="10"
               name="title"
               className=" bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none"
-              placeholder="Product title"
+              placeholder="Product description"
             ></textarea>
           </div>
           <div className=" flex justify-center mb-3">
-            <button className=" bg-yellow-500 w-full text-black font-bold  px-2 py-2 rounded-lg">
+            <button className=" bg-yellow-500 w-full text-black font-bold  px-2 py-2 rounded-lg" onClick={()=>updateProduct()}>
               Update Product
             </button>
           </div>
